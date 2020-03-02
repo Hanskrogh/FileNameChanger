18c0c291bf609512ff24cda217a3e0713c448bb2
hansi <hanskroght@gmail.com>
2020-03-02 16:41:37 +0100

    Add project files.

---
+++ FileNameChanger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileNameChanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
